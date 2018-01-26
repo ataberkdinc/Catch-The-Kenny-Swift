fef8315968f6f0acddc801873263bc01a6dfdee5
Ataberk Dinç <lolomunati@gmail.com>
2018-01-26 15:35:37 +0300

    Initial Commit

---
+++ KennyGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KennyGame
+//
+//  Created by Ataberk Dinç on 26.01.2018.
+//  Copyright © 2018 Ata Games. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
